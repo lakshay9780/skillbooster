7ad316906144891e869213c878f0db93abfb2e4a
Lakshay madhok <112793114+lakshay9780@users.noreply.github.com>
2023-10-27 06:02:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+(function ($) {
+    "use strict";
+    
+    
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+    
+
+
+
+}
+
+
+
+
+
+
+
+)
+function Text(){
+
+    var full_name= document.getElementById("name1").value;
+    var emaiil_= document.getElementById("email1").value;
+    var course_name= document.getElementById("course1").value;
+    alert(full_name,emaiil_,course_name);
+
+
+
+}
+
+
+
+
+
+(jQuery);
+
